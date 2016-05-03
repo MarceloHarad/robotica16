6260b14c8525d10c6d2ba32b853e7e297fd97442
MarceloHarad <marcelogdeandrade@gmail.com>
2016-05-03 15:31:46 -0300

    Final atividade 1

--- visao1/videoplay.py
+++ visao1/videoplay.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture('hall_box_battery.mp4') 
 
@@ -7,7 +8,15 @@ while(True):
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray,100,150,apertureSize = 3)
+    
+    minLineLength = 100
+    maxLineGap = 10
+    lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+    for x1,y1,x2,y2 in lines[0]:
+        cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),2)
+
 
     # Display the resulting frame
     cv2.imshow('frame',frame)
